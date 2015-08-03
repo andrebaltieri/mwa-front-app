5732315e0a88860c58a861f76f28626918b9d5a3
Andre Baltieri <andrebaltieri@hotmail.com>
2015-08-03 19:38:37 -0300

    iniciado repo

---
+++ mwa/www/js/app.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular.module('mwa', ['ionic'])
+        .run(function($ionicPlatform) {
+            $ionicPlatform.ready(function() {
+                if (window.cordova && window.cordova.plugins.Keyboard) {
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                }
+                if (window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+            });
+        });
+})();
